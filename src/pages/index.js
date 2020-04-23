02761a632e3c90f480ba2a7df2d762be63ffce56
Ken Charette <charettekenneth@gmail.com>
2020-04-23 14:57:40 -0400

    Added in chakra ui and initial styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,20 @@
 import React from 'react'
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client'
+import { Heading, List, ListItem, Box } from '@chakra-ui/core'
+import { Helmet}  from 'react-helmet'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const Index = () => {
+    const { site } = useStaticQuery(graphql`
+        {
+            site{
+                siteMetadata{
+                    title
+                }
+            }
+        }
+    `)
+
     const {data, loading, error} = useQuery(gql`
         {
             country(code: "JP"){
@@ -25,18 +38,25 @@ const Index = () => {
         )    
     }
 
+    const { title } = site.siteMetadata
     const { name, capital, languages } = data.country
     return (
-        <div> 
-            <h1> {name}</h1>
-            <p> Capital - {capital}</p>
-            <p>Languages: </p>
-            <ul>
-                {languages.map(language => (
-                    <li key={language.code}>{language.name}</li>
-                ))}
-            </ul>
-        </div>
+        <>
+        <Helmet>
+            <title>{title}</title>
+        </Helmet>
+            <Box as='header' px='6' py='2' bg='blue.200'>{title}</Box>
+                <Box p='4'> 
+                    <Heading mb='2'>{name}</Heading>
+                    <p> Capital - {capital}</p>
+                    <p>Languages: </p>
+                    <List styleType='disc'>
+                        {languages.map(language => (
+                            <ListItem key={language.code}>{language.name}</ListItem>
+                        ))}
+                    </List>
+                </Box>
+        </>
     )
 }
 
